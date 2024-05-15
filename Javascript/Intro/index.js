ed40322481e6a4b5444776f4c72695debfe88f56
Francisco Campos Diaz <hey@francampos.me>
2024-05-14 20:14:42 -0600

    javascript intro:

---
+++ Javascript/Intro/index.js
@@ -0,0 +1,29 @@
+/*
+
+Comentario
+de
+varias
+lineas
+
+*/
+// Comentario de una linea
+
+//Salidas
+
+// alert('hola "" ')
+// alert('')
+// alert(``)
+// alert(2+2) //4
+// alert(10>11) //false
+
+// Escribir en el viewport
+// document.write("Hola Clase de Dev.f")
+
+// por consola
+// console.log("Hola clase")
+
+//Entradas
+// var nombre = prompt("Cual es tu nombre")
+// console.log("Hola ,", nombre)
+
+console.log("Hola " + prompt("Cual es tu nombre?"))
