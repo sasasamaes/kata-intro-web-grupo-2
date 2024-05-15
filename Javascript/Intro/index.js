8d6d128548c1f68a6b01a4d4886944f23b57693a
Francisco Campos Diaz <hey@francampos.me>
2024-05-14 20:57:36 -0600

    ejercicios 1

--- Javascript/Intro/index.js
+++ Javascript/Intro/index.js
@@ -45,11 +45,30 @@ var tipoUndefined;
 // Ejercicio calculadora sencilla
 // Entradas
 var numero1 = prompt("ingrese el primer numero?")
-var numero2 = prompt("ingrese el segundo numero")
+var numero2 = parseInt(prompt("ingrese el segundo numero"))
 
 // Proceso
-var suma = parseInt(numero1) + parseInt(numero2)
+var suma = parseInt(numero1) + numero2
 
 // Salidos
 console.log("La suma de "+ numero1 + " + " + numero2 + " es: " + suma)
+
+// Ejercicios
+/*
+Calculadora de edad:
+Escribe un programa que solicite al usuario su
+año de nacimiento y luego calcule su edad actual y la muestre en pantalla.
+*/
+
+/*
+Calculadora de IMC (Índice de Masa Corporal):
+Pide al usuario su peso y su altura, luego calcula su IMC y muestra el resultado.
+javascript
+*/
+
+/*
+Conversión de temperaturas:
+Solicita al usuario una temperatura en grados Celsius 
+y conviértela a Fahrenheit utilizando la fórmula: F = C * 9/5 + 32.
+*/
 
