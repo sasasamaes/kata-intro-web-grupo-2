ef5a67e0fc964ffc899bf162f0ab82503ce54226
Francisco Campos Diaz <hey@francampos.me>
2024-05-14 20:55:11 -0600

    Javascript basic

--- Javascript/Intro/index.js
+++ Javascript/Intro/index.js
@@ -26,4 +26,30 @@ lineas
 // var nombre = prompt("Cual es tu nombre")
 // console.log("Hola ,", nombre)
 
-console.log("Hola " + prompt("Cual es tu nombre?"))
+// console.log("Hola " + prompt("Cual es tu nombre?"))
+ //variable string
+var nombreVariable = "Francisco"
+//number
+var variableNumero = 13
+//boolean
+var variableBool = true
+//obj
+var variableObjet = {
+  clave: "Valor"
+}
+//null
+var tipoNull = null
+//undefined
+var tipoUndefined;
+
+// Ejercicio calculadora sencilla
+// Entradas
+var numero1 = prompt("ingrese el primer numero?")
+var numero2 = prompt("ingrese el segundo numero")
+
+// Proceso
+var suma = parseInt(numero1) + parseInt(numero2)
+
+// Salidos
+console.log("La suma de "+ numero1 + " + " + numero2 + " es: " + suma)
+
