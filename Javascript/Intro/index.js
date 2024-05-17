ddb9d2d91a8497271a15750c64a8a7f094c26d9b
Francisco Campos Diaz <hey@francampos.me>
2024-05-16 19:10:14 -0600

    ejercicios resueltos 1

--- Javascript/Intro/index.js
+++ Javascript/Intro/index.js
@@ -44,14 +44,14 @@ var tipoUndefined;
 
 // Ejercicio calculadora sencilla
 // Entradas
-var numero1 = prompt("ingrese el primer numero?")
-var numero2 = parseInt(prompt("ingrese el segundo numero"))
+// var numero1 = prompt("ingrese el primer numero?")
+// var numero2 = parseInt(prompt("ingrese el segundo numero"))
 
-// Proceso
-var suma = parseInt(numero1) + numero2
+// // Proceso
+// var suma = parseInt(numero1) + numero2
 
-// Salidos
-console.log("La suma de "+ numero1 + " + " + numero2 + " es: " + suma)
+// // Salidos
+// console.log("La suma de "+ numero1 + " + " + numero2 + " es: " + suma)
 
 // Ejercicios
 /*
@@ -59,16 +59,51 @@ Calculadora de edad:
 Escribe un programa que solicite al usuario su
 año de nacimiento y luego calcule su edad actual y la muestre en pantalla.
 */
+// Entradas
+// var anioNacimiento = prompt("Por favor, ingresa tu anio de nacimiento")
+
+// // Proceso
+// var edadActual = 2024 - parseInt(anioNacimiento)
+
+// // Salida
+// alert("La edad del usuario es: "+ edadActual)
+
 
 /*
 Calculadora de IMC (Índice de Masa Corporal):
 Pide al usuario su peso y su altura, luego calcula su IMC y muestra el resultado.
 javascript
 */
+// Entradas
+// var peso = prompt("Ingresa tu peso en kilogramos")
+// var altura = prompt("Ingrese su altura en metros")
+
+// // Proceso
+// var pesoNum = parseInt(peso)
+// var alturaNum = parseInt(altura)
+
+//  //CALCULO DE IMC
+
+// var imc = pesoNum / (alturaNum * alturaNum)
+
+// // Salida
+// alert("Tu imc es "+imc)
 
 /*
 Conversión de temperaturas:
 Solicita al usuario una temperatura en grados Celsius 
 y conviértela a Fahrenheit utilizando la fórmula: F = C * 9/5 + 32.
 */
+// Entradas
+var celsuis = prompt("Ingrese la temperatura en grados celsuis")
+
+// Convertir de string a number
+var celsuisNum = parseInt(celsuis)
+
+// Proceso
+
+var fahrenheit = (celsuisNum * 9/5) + 32
+
+// Salida
+alert("los grados celsuis de: "+ celsuis +" en fahrenheit son: "+fahrenheit)
 
