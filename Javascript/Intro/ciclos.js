b9ead5aaa623119761c7a9132229749d10490c59
Francisco Campos Diaz <hey@francampos.me>
2024-05-21 20:24:53 -0600

    ciclos

--- Javascript/Intro/ciclos.js
+++ Javascript/Intro/ciclos.js
@@ -5,12 +5,30 @@ while(index < 11){
   index+1
 }
 
-//
+//while
 var arreglo = []
 var userInput;
 
-while(!(userInput =="")){
+while(!(userInput == ""))
+{
   userInput = prompt("Ingrese cualquier caracter")
   arreglo.push(userInput)
 }
 console.log("Introduciste estos valores: "+arreglo)
+
+
+// Do while
+var contador = 0
+do{
+  contador++
+  console.log('Conteo:'+contador)
+} while(contador < 10)
+
+//for
+var contador = 0
+for (var index = 0; index < 10; index++){
+  if(index % 2 ==0) {
+    contador = contador + 1
+    console.log(index + " es multiplo de 2")
+  }
+}
